d6dcbf0a626ef532ea59cf371774100fee689062
OmarAndres7 <oommaarr@misena.edu.co>
2016-12-17 08:46:19 -0500

    commit

---
+++ frontend-pse/app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendPseApp
+ * @description
+ * # frontendPseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendPseApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ frontend-pse/app/scripts/controllers/vista.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendPseApp.controller:VistaCtrl
+ * @description
+ * # VistaCtrl
+ * Controller of the frontendPseApp
+ */
+angular.module('frontendPseApp')
+  .controller('VistaCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
