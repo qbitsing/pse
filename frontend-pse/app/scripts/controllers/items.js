e2666f05a84980173939014fa38df1f17f6bca08
sebastian arias <Sebastian@MacBook-Pro-de-sebastian.local>
2017-01-23 10:58:48 -0500

    estilos

--- frontend-pse/app/scripts/controllers/items.js
+++ frontend-pse/app/scripts/controllers/items.js
@@ -8,23 +8,18 @@
  * Controller of the frontendPseApp
  */
 angular.module('frontendPseApp')
-<<<<<<< HEAD
-.controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario , $state) {
+.controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario , $state ,$timeout) {
 	var modal = null;
-	if(SesionUsuario.ObtenerSesion().rol == "Super Administrador")
-		$state.go('Home');
-=======
-.controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario,$timeout) {
-	var modal = null;
-
+	/*if(SesionUsuario.ObtenerSesion().rol == "Super Administrador")
+		$state.go('Home');*/
 	$scope.panelAnimate='';
-      $scope.pageAnimate='';  
-      $timeout(function () {
+    $scope.pageAnimate='';  
+    $timeout(function () {
          $scope.pageAnimate='pageAnimate';
          $scope.panelAnimate='panelAnimate';
       },100);
 	console.log(SesionUsuario.ObtenerSesion());
->>>>>>> 2f6613c35b7ad4881913a97882da029954d30c46
+
 	$scope.AbrirModal = function(){
 		modal = $uibModal.open({
 			animation: true,
