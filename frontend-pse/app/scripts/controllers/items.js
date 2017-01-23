b96606cca577eb45239f95b6013221d77bb51875
sebastian arias <Sebastian@MacBook-Pro-de-sebastian.local>
2017-01-23 11:11:30 -0500

    comentarios

--- frontend-pse/app/scripts/controllers/items.js
+++ frontend-pse/app/scripts/controllers/items.js
@@ -8,7 +8,7 @@
  * Controller of the frontendPseApp
  */
 angular.module('frontendPseApp')
-<<<<<<< HEAD
+
 .controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario , $state ,$timeout) {
 	var modal = null;
 	/*if(SesionUsuario.ObtenerSesion().rol == "Super Administrador")
@@ -21,7 +21,7 @@ angular.module('frontendPseApp')
       },100);
 	console.log(SesionUsuario.ObtenerSesion());
 
-=======
+
 .controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario,$timeout) {
 	var modal = null;
 
@@ -32,7 +32,7 @@ angular.module('frontendPseApp')
 		$scope.panelAnimate='panelAnimate';
 	},100);
 	console.log(SesionUsuario.ObtenerSesion());
->>>>>>> d0e272368f56880495f04cd89240641d4614dd41
+
 	$scope.AbrirModal = function(){
 		modal = $uibModal.open({
 			animation: true,
