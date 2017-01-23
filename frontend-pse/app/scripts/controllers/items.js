9cbc2a9e0dcb654ff108c4b785c5404d3f6c972e
Omar Plata <oommaarr@misena.edu.co>
2017-01-23 11:15:13 -0500

    Cambios

--- frontend-pse/app/scripts/controllers/items.js
+++ frontend-pse/app/scripts/controllers/items.js
@@ -8,23 +8,6 @@
  * Controller of the frontendPseApp
  */
 angular.module('frontendPseApp')
-<<<<<<< HEAD
-=======
-
-.controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario , $state ,$timeout) {
-	var modal = null;
-	/*if(SesionUsuario.ObtenerSesion().rol == "Super Administrador")
-		$state.go('Home');*/
-	$scope.panelAnimate='';
-    $scope.pageAnimate='';  
-    $timeout(function () {
-         $scope.pageAnimate='pageAnimate';
-         $scope.panelAnimate='panelAnimate';
-      },100);
-	console.log(SesionUsuario.ObtenerSesion());
-
-
->>>>>>> b96606cca577eb45239f95b6013221d77bb51875
 .controller('ItemsCtrl', function ($scope , $uibModal , SesionUsuario,$timeout) {
 	var modal = null;
 	$scope.panelAnimate='';
@@ -33,13 +16,8 @@ angular.module('frontendPseApp')
 		$scope.pageAnimate='pageAnimate';
 		$scope.panelAnimate='panelAnimate';
 	},100);
-<<<<<<< HEAD
 	if(SesionUsuario.ObtenerSesion().rol == "Super Administrador")
 		$state.go('Home');
-=======
-	console.log(SesionUsuario.ObtenerSesion());
-
->>>>>>> b96606cca577eb45239f95b6013221d77bb51875
 	$scope.AbrirModal = function(){
 		modal = $uibModal.open({
 			animation: true,
